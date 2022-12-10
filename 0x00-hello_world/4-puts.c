4ac4a96ba38ef6fafb2aecb71b26e1616847b895
suntechs1990 <ikobosunday@gmail.com>
2022-12-10 11:57:05 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
  #include <stdio.h>
-
 /**
  *
  *  * main - Entry
@@ -7,12 +6,8 @@
  *   * Return: Always 0
  *
  *    */
-
 int main(void)
-
 {
-
-	    puts("\"Programming is like building a multi lingual puzzle);
-	    
-	    return (0);
+ puts("\"Programming is like building a multi lingual puzzle);
+ return (0);
 }
