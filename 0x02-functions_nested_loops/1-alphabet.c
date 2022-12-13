4f2eddf504e62207afef885d02649f4642989487
suntechs1990 <ikobosunday@gmail.com>
2022-12-13 15:53:31 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main-Entry piont
+ * Return:Always 0 (success)
+ */
+int main(void)
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+About nested loop
