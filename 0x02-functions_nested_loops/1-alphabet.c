c33df3c3a453822594bd70a5d4a9c0526b43e184
suntechs1990 <ikobosunday@gmail.com>
2022-12-15 06:31:13 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main-Entry piont
- *
- * Return:Always 0 
+ * print_alphabet - print all alphabet in lowercase
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 	_putchar(letter);
+	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,13 @@
+#include <unistd.h>
 #include "main.h"
-
 /**
- * main-entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return:always 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar(char);
-	return (0);
+	return (write(1, &c, 1));
 }
