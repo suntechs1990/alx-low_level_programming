c8745ac12fe0aacc56e0b05e36c805700a320a2f
suntechs1990 <ikobosunday@gmail.com>
2022-12-21 13:29:18 -0800

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
-
-int _putchar(char c)
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06. C - More pointers, arrays and strings directory.
+ */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
