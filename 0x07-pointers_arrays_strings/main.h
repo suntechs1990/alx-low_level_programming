deb3c286d1e9c3eca7b10f108e528742fe6df96d
suntechs1990 <ikobosunday@gmail.com>
2023-01-02 14:37:10 -0800

    task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: macobilor
+ * description: Header file containing prototypes for all functions
+ * used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
-char *_memset(char *is, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -13,4 +22,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
-
